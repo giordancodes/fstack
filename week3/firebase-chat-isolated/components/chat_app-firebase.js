0e8f599013112c60637c0b0f0b43dea0c3d3602e
Giordan Battaglin <gio@giordan.ca>
2016-10-11 23:59:54 -0400

    one step forward, and one backwards

--- week3/firebase-chat-isolated/components/chat_app-firebase.js
+++ week3/firebase-chat-isolated/components/chat_app-firebase.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import firebase from 'firebase';
 
 
-var ChatApp = React.createClass({
+const ChatApp = React.createClass({
   getInitialState() {
     return {
       newMessage: "",
@@ -15,40 +14,49 @@ var ChatApp = React.createClass({
 
   render() {
     if (!this.state.loggedIn) {
-      return <div className="splash">
-        <form onSubmit={ (e) => {e.preventDefault() } }>
-        	<input type='text' placeholder="Enter your name to start blabbing!" onChange={ this.setCurrentUser } />
-        	<button onClick={ this.login }>Login</button>
-        </form>
-      </div>
+      return (
+				<div className="splash">
+					<form onSubmit={ (e) => e.preventDefault() }>
+						<input type='text' placeholder="Enter your name to start blabbing!" onChange={ this.setCurrentUser } />
+						<button onClick={ this.login }>Login</button>
+					</form>
+				</div>)
     } else {
-      return <div>
-        <h1>Welcome, { this.state.currentUser }</h1>
+      return (
+				<div>
+					<h1>Welcome, { this.state.currentUser }</h1>
 
-        { Object.keys(this.state.messages).map((id) => {
-					var message = this.state.messages[id];
-					var author = this.state.messages[id].author;
-          return <div key={ id } className="message">
-            <div className='author'>{ message.author }:</div>
-            <div>{ message.message }</div>
-						<div>
-							{ author == this.state.currentUser ?
-								<button onClick={ () => this.deleteMessage(id) }>
-									delete?
-								</button> : null}
+					{ Object.keys(this.state.messages).map((id) => {
+						
+						const message = this.state.messages[id];
+						const author = this.state.messages[id].author;
+						return <div key={ id } className="message">
+							<div className='author'>{ message.author }:</div>
+							<div>{ message.message }</div>
+							<div>
+								{ author == this.state.currentUser ?
+									<div>
+										<button onClick={ () => this.deleteMessage(id) }>
+											delete?
+										</button>
+										{/* <button onClick={ () => this.editMessage(id) }>
+											edit?
+										</button> */}
+									</div>
+
+									: null}
 						</div>
           </div>
         })}
         
-        
         <div className='newMessage'>
-          <form onSubmit={(event) => event.preventDefault() }>
+          <form onSubmit={ (e) => e.preventDefault() }>
           	<input type='text' value={ this.state.newMessage } onChange={ this.updateNewMessage } placeholder="Fill some space" />
           	<button onClick={ this.postMessage }
           					onSubmit={ this.postMessage }>Add Message</button>
           </form>
         </div>
-      </div>
+      </div>)
     }
   },
 
@@ -66,7 +74,7 @@ var ChatApp = React.createClass({
 
   postMessage() {
 		
-    var newMessage = { 
+    const newMessage = { 
 			author: this.state.currentUser,
 			message: this.state.newMessage 
 		}
@@ -75,14 +83,18 @@ var ChatApp = React.createClass({
 		this.setState({ newMessage: ""})
   },
 	
-	onDelete(id){
-		this.deleteMessage(id);
-	},
-	
 	deleteMessage(id){
 		if(confirm("Are you friggin' sure?")){
 			this.firebaseRef.child(id).remove();
 		}
+	},
+	
+	editMessage(id){
+		const messages = this.state.messages,
+					msg = messages[id].message,
+					updatedMessage = prompt('try again:', 'excrete poetry here');
+		console.log(msg);
+		console.log(updatedMessage);
 	},
 	
 	componentDidUpdate(){
@@ -96,14 +108,14 @@ var ChatApp = React.createClass({
 		this.firebaseRef.on("child_added", (dataSnapshot) => {
 //			console.log(dataSnapshot.key, dataSnapshot.val());
 			
-			var messages = this.state.messages;
+			const messages = this.state.messages;
 			messages[dataSnapshot.key] = dataSnapshot.val();
 			
 			this.setState({messages});
 		});
 		
 		this.firebaseRef.on('child_removed', (dataSnapshot) => {
-			var messages = this.state.messages;
+			const messages = this.state.messages;
 			
 			delete messages[dataSnapshot.key];
 			
@@ -113,7 +125,7 @@ var ChatApp = React.createClass({
 	
 })
 
-// for (var property in messages){
+// for (const property in messages){
 //					console.log(property);
 //					messages[property];
 //				}
