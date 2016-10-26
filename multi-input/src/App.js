e3dae14bccd088282428294e5d05f076b6d600af
Giordan Battaglin <gio@giordan.ca>
2016-10-25 20:34:37 -0400

    ejected multi-input

--- multi-input/src/App.js
+++ multi-input/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+
+import Footer from './Footer';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       form: {
         name: '',
@@ -24,21 +25,30 @@ class App extends Component {
           <h1>multi-input</h1>
         </div>
         <form onSubmit={ (e) => e.preventDefault() }>
-          <input name='name' value={ this.state.form.name } onChange={ this.updateField } type="text"/>Name
-          <input name='email' value={ this.state.form.email } onChange={ this.updateField } type="text"/>Email
-          <input name='colour' value={ this.state.form.colour } onChange={ this.updateField } type="text"/>Fav Colour
-          <select name='gender' value={ this.state.form.gender } onChange={ this.updateField } >
-            <option>Femme</option>
-            <option>Homme</option>
-            <option>Autre</option>
-          </select>
+          <div>
+            <input name='name' value={ this.state.form.name } onChange={ this.updateField } type="text"/></div>
+            <label htmlFor="name">Name</label>
+          <div>
+            <input name='email' value={ this.state.form.email } onChange={ this.updateField } type="text"/></div>
+            <label htmlFor="email">Electronic Mail Address</label>
+          <div>
+            <input name='colour' value={ this.state.form.colour } onChange={ this.updateField } type="text"/></div>
+            <label htmlFor="colour">Shoe Colour</label>
+          <div>
+            <select name='gender' value={ this.state.form.gender } onChange={ this.updateField } >
+              <option>Femme</option>
+              <option>Homme</option>
+              <option>Autre</option>
+            </select>
+            <label htmlFor="gender">Gender</label>         
+          </div>
         </form>
+        <Footer />
       </div>
     );
   }
 
   updateField = (e) => {
-    console.log(e.target.name);
     let form = this.state.form;
 
     form[e.target.name] = e.target.value;
---
+++ multi-input/src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+
+	render(){
+		return (
+      <div>
+				<footer>
+					<h4>built with the JSX</h4>
+				</footer>
+			</div>
+		)
+	}
+
+}
+
+export default Footer;
