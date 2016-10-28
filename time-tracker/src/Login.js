505741eebf707a86ac4b449abb752fce7b8737ba
Giordan Battaglin <gio@giordan.ca>
2016-10-28 16:51:54 -0400

    added a nice hello to email in login component

--- time-tracker/src/Login.js
+++ time-tracker/src/Login.js
@@ -19,32 +19,34 @@ class Login extends Component {
     return (
       <div>
         <h1>Identify Yourself</h1>
-
+				<p>{ 
+					(this.state.form.email) ? `Hello, ${this.state.form.email}` : null 
+				}</p>
         <form onSubmit={ (e) => e.preventDefault }>
-        	<label htmlFor="email">email</label>
+        	<label 	htmlFor="email">email</label>
           <input 	type="text" 
           				placeholder="email"
           				id="email"
           				value={ this.state.form.email }
           				onChange={ this.updateField } />
-          <label htmlFor="password">password</label>
+          <label 	htmlFor="password">password</label>
           <input 	type="password"
           				placeholder="password"
           				id="password"
           				value={ this.state.form.password }
           				onChange={ this.updateField } />
+          <label htmlFor="login">Login</label>
           <input 	type="radio"
-          				value='login'
+          				id='login'
 									checked={ this.state.mode === 'login' }
           				onChange={ this.setMode }
           				 />
-          				
+          <label htmlFor="signup">Sign Up</label>
 				 	<input 	type="radio"
-          				value='signup'
+          				id='signup'
 									checked={ this.state.mode === 'signup' }
           				onChange={ this.setMode }
           				 />
-          				
         </form>
       </div>
     );
@@ -57,7 +59,7 @@ class Login extends Component {
   }
 
   setMode = (e) => {
-  	this.setState({mode: e.target.value})
+  	this.setState({mode: e.target.id})
   }
 }
 
