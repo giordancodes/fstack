f9c8fa6a943a30c7392b18f02043f8748688c47a
Giordan Battaglin <gio@giordan.ca>
2016-11-04 21:42:20 -0400

    Error: Can't set headers after they are sent. for all(err) in node server, didnt occur on other machine

--- week6-backend/express-skeletal/static-server.js
+++ week6-backend/express-skeletal/static-server.js
@@ -9,8 +9,29 @@ app.get('/api/:str', (req, res) =>{
 	res.send(`You requested /api/${req.params.str}`);
 });
 
+app.get('*.css', (req, res) =>{
+	res.sendFile(`${__dirname}/public/public.css`, (err) =>{
+		res.status(404);
+		res.send("The requested file has not been found.");
+	});
+});
+
 app.get('*.jpg', (req, res) =>{
 	res.sendFile(`${__dirname}/public/skinner.jpg`, (err) =>{
+		res.status(404);
+		res.send("The requested file has not been found.");
+	});
+});
+
+app.get('*.js', (req, res) =>{
+	res.sendFile(`${__dirname}/public/dat.js`, (err) =>{
+		res.status(404);
+		res.send("The requested file has not been found.");
+	});
+});
+
+app.get('*.json', (req, res) =>{
+	res.sendFile(`${__dirname}/public/public.json`, (err) =>{
 		res.status(404);
 		res.send("The requested file has not been found.");
 	});
